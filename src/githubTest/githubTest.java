3d221652dfb7154fdee6f9d7da5f7dfdfca4ba03
Java <Java@DESKTOP-TQ00GH9>
2018-12-02 16:18:15 +0800

    Second github

--- src/githubTest/githubTest.java
+++ src/githubTest/githubTest.java
@@ -3,6 +3,8 @@ package githubTest;
 public class githubTest {
 	public static void main(String[] args) {
 		System.out.println("Hello Github!!");
+		System.out.println("Hello Github!!");
+		System.out.println("Hello Github!!");
 	}
 
 }
